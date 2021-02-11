5714b01c6ad2d58011345df218df310e799c6c94
MrSpiritualMomo <76740889+MrSpiritualMomo@users.noreply.github.com>
2021-02-11 20:40:06 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,25 @@
+class Slingshot{
+    constructor(bodyA,pointB)
+        {var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            length:100,
+            stiffness:1
+        }
+    this.slingshot = Constraint.create(options)
+    this.pointB = pointB
+     World.add(world,this.slingshot);   
+        }
+        fly(){
+            this.slingshot.bodyA = null
+        }
+        display(){
+            if(this.slingshot.bodyA){
+                
+            
+        var pointA = this.slingshot.bodyA.position
+        var pointB = this.pointB
+        }
+        //line(pointA.x,pointA.y,pointB.x,pointB.y)
+        }
+}
